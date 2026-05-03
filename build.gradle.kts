b637348c772e4263b7c48cac50fcfffd5658fc78
François SP <francois.sp@ik.me>
2026-05-04 00:08:36 +0200

    feat: update build.gradle to add publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     id("java-library")
     id("org.openjfx.javafxplugin") version "0.1.0"
     id("maven-publish")
+    id("signing")
+    id("com.gradleup.nmcp") version "0.1.4"
 }
 
 group = "io.github.francois389"
@@ -13,13 +15,10 @@ repositories {
     mavenCentral()
 }
 
-
 val springBootVersion = "4.0.5"
 
 dependencies {
-    // BOM Spring Boot pour aligner toutes les versions
     api(platform("org.springframework.boot:spring-boot-dependencies:$springBootVersion"))
-
     api("org.springframework.boot:spring-boot-starter")
 }
 
@@ -29,6 +28,7 @@ kotlin {
 
 java {
     withSourcesJar()
+    withJavadocJar()
 }
 
 javafx {
@@ -40,6 +40,45 @@ publishing {
     publications {
         create<MavenPublication>("maven") {
             from(components["java"])
+
+            pom {
+                name.set("JavaSpringFX")
+                description.set("Librairie Kotlin intégrant Spring Boot et JavaFX pour le développement d'applications desktop MVVM")
+                url.set("https://github.com/Francois389/JavaSpringFX")
+
+                licenses {
+                    license {
+                        name.set("Apache License 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("francois389")
+                        name.set("François")
+                        email.set("francois-sp@gmx.fr")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/Francois389/JavaSpringFX.git")
+                    developerConnection.set("scm:git:ssh://github.com/Francois389/JavaSpringFX.git")
+                    url.set("https://github.com/Francois389/JavaSpringFX")
+                }
+            }
         }
     }
+
+    nmcp {
+        centralPortal {
+            username = project.findProperty("mavenCentralUsername") as String
+            password = project.findProperty("mavenCentralPassword") as String
+            publishingType = "USER_MANAGED"
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["maven"])
 }
